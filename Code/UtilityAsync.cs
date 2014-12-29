85baffb97531ed813a211cc4149e60713dd27def
Pasquale Iaquinta <pasqualeiaquinta@hotmail.com>
2014-12-29 13:36:40 +0100

    1

---
+++ Code/UtilityAsync.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Code
+{
+    public class UtilityAsync
+    {
+
+        public static void Execute(Action action)
+        {
+            try
+            {
+                var asyncExecute = new AsyncExecute();
+                asyncExecute.Start(action);
+
+            }
+            catch (Exception ex)
+            {
+                UtilityError.Write(ex);
+            } 
+        }
+
+        private class AsyncExecute
+        {
+            Gizmox.WebGUI.Forms.Timer timer = null;
+            public AsyncExecute(int interval = 250)
+            {
+                try
+                {
+                    timer = new Gizmox.WebGUI.Forms.Timer();
+                    timer.Interval = interval;
+                    timer.Start();
+
+                }
+                catch (Exception ex)
+                {
+                    UtilityError.Write(ex);
+                } 
+            }
+
+            public void Start(Action action)
+            {
+                try
+                {
+                    var callback = new AsyncCallback(Callback);
+                    action.BeginInvoke(callback, null);
+
+                }
+                catch (Exception ex)
+                {
+                    UtilityError.Write(ex);
+                } 
+            }
+
+            private void Callback(IAsyncResult ar)
+            {
+                try
+                {
+                    timer.Stop();
+                    timer = null;
+
+                }
+                catch (Exception ex)
+                {
+                    UtilityError.Write(ex);
+                } 
+            }
+
+        }
+
+    }
+}
