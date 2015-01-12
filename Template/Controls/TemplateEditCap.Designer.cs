77b50ce567aae43a8ea3faff2c0d07a918bc54a5
Pasquale Iaquinta <pasqualeiaquinta@hotmail.com>
2015-01-12 22:38:44 +0100

    cap control

---
+++ Template/Controls/TemplateEditCap.Designer.cs
@@ -0,0 +1,73 @@
+using Gizmox.WebGUI.Forms;
+using Gizmox.WebGUI.Common;
+
+namespace Library.Template.Controls
+{
+    partial class TemplateEditCap
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Visual WebGui UserControl Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.editControl = new Library.Controls.TextBox();
+            this.SuspendLayout();
+            // 
+            // editControl
+            // 
+            this.editControl.Anchor = ((Gizmox.WebGUI.Forms.AnchorStyles)((((Gizmox.WebGUI.Forms.AnchorStyles.Top | Gizmox.WebGUI.Forms.AnchorStyles.Bottom) 
+            | Gizmox.WebGUI.Forms.AnchorStyles.Left) 
+            | Gizmox.WebGUI.Forms.AnchorStyles.Right)));
+            this.editControl.AutoValidate = Gizmox.WebGUI.Forms.AutoValidate.EnablePreventFocusChange;
+            this.editControl.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.editControl.Behavior = Library.Controls.TextBox.TypeBehavior.Text;
+            this.editControl.BorderWidth = new Gizmox.WebGUI.Forms.BorderWidth(0);
+            this.editControl.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.editControl.Location = new System.Drawing.Point(100, 1);
+            this.editControl.Mask = "---";
+            this.editControl.Name = "editControl";
+            this.editControl.ReadOnly = false;
+            this.editControl.Size = new System.Drawing.Size(351, 27);
+            this.editControl.TabIndex = 0;
+            this.editControl.Text = null;
+            this.editControl.Value = null;
+            // 
+            // TemplateEditNumeric
+            // 
+            this.Controls.Add(this.editControl);
+            this.Text = "TemplateEditNumeric";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Library.Controls.TextBox editControl;
+
+        
+
+
+
+    }
+}
---
+++ Template/Controls/TemplateEditCap.cs
@@ -0,0 +1,53 @@
+#region Using
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+
+using Gizmox.WebGUI.Common;
+using Gizmox.WebGUI.Forms;
+using Library.Interfaces;
+using Library.Code;
+using Gizmox.WebGUI.Common.Interfaces;
+using Library.Controls;
+
+
+#endregion
+
+namespace Library.Template.Controls
+{
+    public partial class TemplateEditCap : EditControl
+    {
+
+        public TemplateEditCap()
+        {
+            InitializeComponent();
+            try
+            {
+                base.MaskControl = editControl;
+                editControl.Behavior = Library.Controls.TextBox.TypeBehavior.Cap;
+            }
+            catch (Exception ex)
+            {
+                UtilityError.Write(ex);
+            } 
+        }
+
+        public new string Value
+        {
+            get
+            {
+                return (string)editControl.Value; ;
+            }
+            set
+            {
+                editControl.Value = value;
+            }
+        }
+
+
+    }
+}
