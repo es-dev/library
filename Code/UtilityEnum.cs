3ee9ea9f0d3d46e6b7e849d47b0bf4fe1c8fd0f0
Pasquale Iaquinta <pasqualeiaquinta@hotmail.com>
2015-02-04 20:53:24 +0100

    utilityenum

---
+++ Code/UtilityEnum.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Code.Enum
+{
+    public class UtilityEnum
+    {
+       
+    }
+
+    public enum TypeStato
+    {
+        None,
+        Critical,
+        Warning,
+        Normal
+    }
+
+    public enum TypeMessage
+    {
+        None,
+        Info,
+        Question,
+        Alert,
+        Confirm
+    }
+
+    public enum JQTypePosition
+    {
+        Center,
+        Docked
+    }
+
+    public enum TypeDirection
+    {
+        Horizontal,
+        Vertical
+    }
+
+    public enum TypeBehavior
+    {
+        Text,
+        Password,
+        Numeric,
+        Decimal,
+        CodiceFiscale,
+        PartitaIva,
+        Email,
+        Cap
+    }
+
+}
