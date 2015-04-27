2da7fe2e5d816787aa778256c380996babffc57c
Pasquale Iaquinta <pasqualeiaquinta@hotmail.com>
2015-04-27 20:59:20 +0200

    bug

---
+++ Template/MVVM/TemplateViewTypeModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Template.MVVM
+{
+    public class TemplateViewTypeModel<TView, TItem, TDto> : TemplateViewModel<TView, TItem, Type, TDto>
+    {
+        public TemplateViewTypeModel()
+            : base()
+        {
+
+        }
+    }
+}
